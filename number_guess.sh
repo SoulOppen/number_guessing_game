0119a57b258d8bfb69287fe82e5ccc2a55aaddf2
Ariel Oppenheimer <arieloppenheimer@outlook.cl>
2024-02-23 02:51:53 +0000

    fix:echos

--- number_guess.sh
+++ number_guess.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 SECRET_NUMBER=$(($RANDOM%1000+1))
-PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo -e "\nEnter your username:"
 read USERNAME
-USERNAME_RESULT="$($PSQL "SELECT COUNT(game_id),MIN(plays) FROM games RIGHT JOIN users USING(user_id) WHERE name='$USERNAME' GROUP BY name")"
+USERNAME_RESULT="$($PSQL "SELECT COUNT(game_id), MIN(plays) FROM games INNER JOIN users USING(user_id) WHERE name='$USERNAME' GROUP BY name")"
 if [[ -z $USERNAME_RESULT ]]
 then
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   INSERT_NAME="$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")"
 else
-  echo $USERNAME_RESULT| while IFS="|" read GAMES BEST_GAME
+  echo $USERNAME_RESULT | while IFS="|" read GAMES BEST_GAME
   do
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES games, and your best game took $BESTGAME guesses."
+    echo -e "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST_GAMES guesses."
   done
 fi
+USERNAME_ID="$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")"
 NUMBER=0
 PLAY=0
 while [[ SECRET_NUMBER -ne NUMBER ]] 
@@ -23,10 +24,14 @@ do
   read NUMBER
   if [[ $NUMBER =~ ^[0-9]+$ ]]
   then
-    if [[ $SECRET_NUMBER -gt $NUMBER ]]; 
+    if [[ $SECRET_NUMBER == $NUMBER ]]
+    then
+        INSERT_GAME="$($PSQL "INSERT INTO games(plays,user_id) VALUES('$PLAY',$USERNAME_ID)")"
+        break
+    elif [[ $SECRET_NUMBER -gt $NUMBER ]]
     then
         echo "It's higher than that, guess again:"
-    elif [[ $SECRET_NUMBER -lt $NUMBER ]]; 
+    elif [[ $SECRET_NUMBER -lt $NUMBER ]] 
     then
         echo "It's lower than that, guess again:."
     fi
@@ -34,3 +39,4 @@ do
     echo "That is not an integer, guess again:"
   fi
 done
+echo  "You guessed it in $PLAY tries. The secret number was $SECRET_NUMBER. Nice job!"
